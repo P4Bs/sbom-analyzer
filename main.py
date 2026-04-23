a487f604cb8103b68923d65db4b9950f9593f66c
Alvaro Bermudez Gamez <alvarobermudezgamez@gmail.com>
2026-04-23 19:48:16 +0200

    fixed prints

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ def main():
 
         for res in results:            
             print(f"[{res['severity'].upper()}] {res['id']} detectado en el paquete '{res['package']}'")
-            print(f"    ↳ Descripción: {res['description']} ({res['id']})")
-            print(f"    ↳ Tasa de interés: {res['interest_rate']}")
+            print(f"    ↳ Descripcion: {res['description']} ({res['id']})")
+            print(f"    ↳ Tasa de interes: {res['interest_rate']}")
             print(f"    ↳ Probabilidad de ataque futuro:   {res['attack_probability']} %\n")
             
 
     except FileNotFoundError as e:
-        print(f"Error: No se encontró el archivo. Detalles: {e}")
+        print(f"Error: No se encontro el archivo. Detalles: {e}")
         sys.exit(1)
     except Exception as e:
         print(f"Error al procesar el reporte de seguridad: {e}")
